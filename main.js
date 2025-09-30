0176ce56c0519b1ea8235f42fea7284ba09a8a0c
ZenPandaOoO <hubbaandmilitaries@gmail.com>
2025-09-30 15:22:59 -0400

    added clear button. display is showing buttons pressed

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const input = ["7", "8", "9", "/", "4", "5", "6", "-", 
-    "1", "2", "3", "+", ".", "0", "+", "X"];
+    "1", "2", "3", "X", ".", "0", "+", "="];
 
 const buttons = document.getElementById("buttons");
+const display = document.getElementById("display");
+const clear = document.getElementById("clear");
+console.log(clear);
+
 
 for (let i = 0; i < 16; i++) {
     const btn = document.createElement("button");
     btn.textContent = input[i];
     btn.className = "button-text";
+
+    btn.addEventListener("click", () => {
+        display.textContent += btn.textContent;
+    })
+
     buttons.appendChild(btn);
 }
 
-const display = document.getElementById("display");
-display.textContent = "Killkulator";
+clear.textContent = "CLEAR";
+clear.addEventListener("click", () => {
+    console.log("clear!!");
+    display.textContent = "";
+})
 
 
